13166a388fd673f93b456067b259aa08e14b8b24
Marcos Paulo Diniz <34491113+mdiniz97@users.noreply.github.com>
2020-07-23 16:32:18 -0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,9 +4,22 @@ setup(
     name="ansibleawx-client",
     version="0.1.0",
     description="Ansible AWX Client",
+    license='MIT',
+    url='https://github.com/mdiniz97/AnsibleAWX-Client/',
+    author='Marcos Diniz',
+    author_email='marcosdinizpaulo@gmail.com',
     packages=find_packages(),
     include_package_data=True,
     install_requires=[
         'requests',
     ],
+    classifiers=[
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+        'Topic :: Software Development :: Libraries :: Python Modules'
+    ]
 )
